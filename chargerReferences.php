665d7d9370a75687397bfda8bca2f457f1fb3eba
DidierNicolas <ndidier.parriat@gmail.com>
2018-01-09 17:54:29 +0100

    Commit changement CSS

--- chargerReferences.php
+++ chargerReferences.php
@@ -1,18 +1,16 @@
 <?php 
     include('connection.php'); 
     
-    include('connection.css');
-
     if(mysqli_connect_errno()){
         printf('Echec de la connexion: %s\n"', mysqli_connect_error());
     }
 
-    $query = "select * from references order by idR";
+    $query = "select * from Reference order by idR";
     $div = "";
     
     if($result = mysqli_query($link,$query)){
         while($row = mysqli_fetch_array($result,MYSQLI_NUM)){
-            $div = "<div>".$row["idR"]."</div>";
+            $div = "<div>".$row[0]."</div>";
         }
         mysqli_free_result($result);
     }
--- vendre.php
+++ vendre.php
@@ -1,12 +1,11 @@
-<div>
-    <h1></h1>
+<div class="formulaire">
     <form action="">
-        <input type="text" value="" placeholder="Titre du manga">
-        <input type="text" placeholder="Auteur du manga">
-        <input type="text" placeholder="Catégorie du manga">
-        <input type="number" placeholder="Nombre de pages">
-        <input type="text" placeholder="Prix du mange">
-        <button type="submit">Envoyer</button>
+        <input class="input" type="text" value="" placeholder="Titre du manga">
+        <input class="input" type="text" placeholder="Auteur du manga">
+        <input class="input" type="text" placeholder="Catégorie du manga">
+        <input class="input" type="number" placeholder="Nombre de pages">
+        <input class="input" type="text" placeholder="Prix du mange">
+        <button class="button" type="submit">Envoyer</button>
         
     </form>
 </div>
