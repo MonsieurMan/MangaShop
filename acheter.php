bc3fa1b9aab12223fb33c20ebd48b54c450fb9a0
l.tambarin@gmail.com <l.tambarin@gmail.com>
2018-01-09 17:21:23 +0100

    Ajout de la récupération des articles

--- acheter.php
+++ acheter.php
@@ -1,4 +1,3 @@
-
-<div> 
-
-</div>
+<?php
+    include('chargerReferences.php');
+?>
--- chargerReferences.php
+++ chargerReferences.php
@@ -1,3 +1,23 @@
 <?php 
     include('connection.php'); 
+    
+    include('connection.css');
+
+    if(mysqli_connect_errno()){
+        printf('Echec de la connexion: %s\n"', mysqli_connect_error());
+    }
+
+    $query = "select * from references order by idR";
+    $div = "";
+    
+    if($result = mysqli_query($link,$query)){
+        while($row = mysqli_fetch_array($result,MYSQLI_NUM)){
+            $div = "<div>".$row["idR"]."</div>";
+        }
+        mysqli_free_result($result);
+    }
+
+    mysqli_close($link);
+
+    echo $div;
 ?>
