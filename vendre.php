2e7eebfec4c82412d6d0aa77bd7e63f5c70ef5d5
DidierNicolas <ndidier.parriat@gmail.com>
2018-01-10 14:53:14 +0100

    Modification page vendre

--- vendre.php
+++ vendre.php
@@ -1,11 +1,16 @@
-<div class="formulaire">
-    <form action="">
-        <input class="input" type="text" value="" placeholder="Titre du manga">
-        <input class="input" type="text" placeholder="Auteur du manga">
-        <input class="input" type="text" placeholder="Catégorie du manga">
-        <input class="input" type="number" placeholder="Nombre de pages">
-        <input class="input" type="text" placeholder="Prix du mange">
-        <button class="button" type="submit">Envoyer</button>
-        
+<div class="opacity">
+</div>
+<div class="vendre">
+    
+    <form class="form" action="">
+        <p class="titreV">Mise a prix de votre manga :</p>
+        <div class="champs">
+            <input class="input" type="text" value="" placeholder="Titre du manga">
+            <input class="input" type="text" placeholder="Auteur du manga">
+            <input class="input" type="text" placeholder="Catégorie du manga">
+            <input class="input" type="number" placeholder="Nombre de pages">
+            <input class="input" type="text" placeholder="Prix du mange">
+            <button class="button input" type="submit">Envoyer</button>
+        </div>
     </form>
 </div>
